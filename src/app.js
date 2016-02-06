23ab2bbf9a8081d96ad9f1da754d1a66fc42cafb
Luke Davis <luke@peopleofthebit.com>
2016-02-05 17:49:11 -0800

    extract model

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 var style = require('./style.js');
+var model = require('./model.js');
 
 var controller = {};
 
@@ -163,47 +164,6 @@ controller.love = function(locus, value) {
   return true;
 }
 
-var model = {
-  appName: "conflexian",
-  title: "conflexian",
-
-  bounds: {},
-  paper: {},
-
-  interests: [
-    "Luke"
-    ],
-
-  links: [
-  {type:"link", text:"linkedin", url:"http://linkedin.com/in/lucaswadedavis/en/", image:"./images/linkedin-128-black.png"},
-  {type:"link", text:"twitter", url:"http://twitter.com/lukedavis", image:"./images/twitter-128-black.png"},
-  {type:"link", text:"github", url:"http://github.com/lucaswadedavis", image:"./images/github-128-black.png"},
-  {type:"link", text:"blog", url:"http://lucaswadedavis.com", image:"./images/blogger-128-black.png"},
-  ],
-
-  images128: [
-  {type:"image128",text:"me",url:"#",image:"./images/headshot-128.png"}
-  //{type:"image128",text:"mandala time",url:"http://holograf.io",image:"./images/holograf128.png"},
-  //{type:"image128",text:"mandala time",url:"http://lucaswadedavis.github.io/coloroordinates/",image:"./images/coloroordinates128.png"},
-  //{type:"image128",text:"mandala time",url:"http://fauxpoe.com",image:"./images/fauxpoe128.png"}
-  ],
-
-    images256: [
-    {type:"image256",text:"me",url:"#",image:"./images/headshot-256.png"}
-  //{type:"image256",text:"mandala time",url:"http://holograf.io",image:"./images/holograf256.png"},
-  //{type:"image256",text:"mandala time",url:"http://lucaswadedavis.github.io/coloroordinates/",image:"./images/coloroordinates256.png"},
-  //{type:"image256",text:"mandala time",url:"http://fauxpoe.com",image:"./images/fauxpoe256.png"}
-  ],
-
-    icons: [
-    {type:"icon", image:"./images/twitter-128.png", url:"http://twitter.com/lukedavis"},
-    {type:"icon", image:"./images/github-128.png", url:"http://github.com/lucaswadedavis"},
-    {type:"icon", image:"./images/linkedin-128.png", url:"http://linkedin.com/in/lucaswadedavis/en/"},
-    {type:"icon", image:"./images/blogger-128.png", url:"http://lucaswadedavis.com"}
-  ]
-
-};
-
 var view = {};
 
 view.init = function () {  
@@ -260,15 +220,6 @@ view.row = function(colspans, model_content_index) {
   }
   d += "</div>";
   return d;
-};
-
-view.style = function() {
-  var color = davis.pick(model.colors);
-  var style = "style='";
-  style += "background-color:" + color.rgb + ";";
-  style += "color:" + color.text + ";";
-  style += "'";
-  return style;
 };
 
 view.content = function() {
--- src/build.js
+++ src/build.js
@@ -60,210 +60,170 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	var style = __webpack_require__(2);
+	var model = __webpack_require__(3);
 
 	var controller = {};
 
 	controller.init = function() {
-	    if (!model.stock) {
-	      model.stock = {};
-	      model.stock.colors = [];
-	      model.stock.greys = [];
-	      model.stock.widths = [];
-	      model.stock.borderRadii = [];
-	      model.stock.borderSpacings = [];
-	      model.stock.borderWidths = [];
-	      model.stock.frames = [];
-	      model.stock.content = [];
-	    }
-	    this.styles();
-	    this.cssClasses();
-	    this.content();
+	  if (!model.stock) {
+	    model.stock = {};
+	    model.stock.colors = [];
+	    model.stock.greys = [];
+	    model.stock.widths = [];
+	    model.stock.borderRadii = [];
+	    model.stock.borderSpacings = [];
+	    model.stock.borderWidths = [];
+	    model.stock.frames = [];
+	    model.stock.content = [];
+	  }
+	  this.styles();
+	  this.cssClasses();
+	  this.content();
 
-	  };
+	};
 
 	controller.content = function() {
-	    model.content = [];
-	    var x = 1;
-	    var d = "";
+	  model.content = [];
+	  var x = 1;
+	  var d = "";
 
-	    x = _.random(0, 12);
-	    for (var i = 0; i < x; i++) {
-	      d = "";
-	      d += "<h1 class='text-center'>" + davis.pick(model.interests).toUpperCase() + "</h1>";
-	      if (davis.random(10) > 3) {
-	        d += "<p class='text-center'>" + davis.randomWord(12) + "</p>";
-	      }
-	      model.content.push(d);
-	    }
-
-
-	    x = _.random(0, 3);
-	    for (var j = 0; j < x; j++) {
-	      d = "";
-	      d += "<h2 class='text-center'>Links</h3>";
-	      var divClass = davis.pick(model.cssClasses).name;
-	      for (var i = 0; i < model.links.length; i++) {
-	        d += "<a href='" + model.links[i].url + "'><div class='text-center link " + divClass + "' >"+model.links[i].text+"</div></a>";
-	      }
-	      model.content.push(d);
-
-	    }
-
-	    x = _.random(0, 3);
-	    for (var j = 0; j < x; j++) {
-	      d = "";
-	      d += "<h2 class='text-center'>" + davis.random(100) + "%</h2>";
-	      for (var i = 0; i < _.random(1); i++) {
-	        d += "<p class='text-center'>" + davis.randomWord(10 + davis.random(8)) + "</p>";
-	      }
-	      model.content.push(d);
-
-	    }
-
-	    x = _.random(0, 3);
-	    for (var j = 0; j < x; j++) {
-	      d = "";
-	      d += "<h2 class='text-center'>" + davis.random(1000) + " " + davis.pick(model.interests) + "</h2>";
-	      model.content.push(d);
-
-	    }
-
-	    x = _.random(0, 3);
-	    for (var j = 0; j < x; j++) {
-	      d = "";
-	      d += "<h3 class='text-center'>" + davis.pick(model.interests) + "</h3>";
-	      for (var i = 0; i < davis.random(4); i++) {
-	        d += "<p>" + davis.randomWord(10 + davis.random(80)) + "</p>";
-	      }
-	      model.content.push(d);
-
-	    }
-
-	    x = _.random(0, 6);
-	    for (var j = 0; j < x; j++) {
-	      d = "";
-	      d += "<h3 class='text-center'>";
-	      var links = [];
-	      for (var i = 0; i < model.links.length; i++) {
-	        links.push("<a href='"+model.links[i].url+"'>"+model.links[i].text+"</a>");
-	      }
-	      d += links.join(" | ");
-	      d += "</h3>";
-	      model.content.push(d);
-
-	    }
-
+	  x = _.random(0, 12);
+	  for (var i = 0; i < x; i++) {
 	    d = "";
-	    for (var i=0;i<model.icons.length;i++){
-	      var icon = model.icons[i];
-	      d += "<a href='"+icon.url+"'><img src='" + icon.image + "' ></a>";
+	    d += "<h1 class='text-center'>" + davis.pick(model.interests).toUpperCase() + "</h1>";
+	    if (davis.random(10) > 3) {
+	      d += "<p class='text-center'>" + davis.randomWord(12) + "</p>";
+	    }
+	    model.content.push(d);
+	  }
+
+
+	  x = _.random(0, 3);
+	  for (var j = 0; j < x; j++) {
+	    d = "";
+	    d += "<h2 class='text-center'>Links</h3>";
+	    var divClass = davis.pick(model.cssClasses).name;
+	    for (var i = 0; i < model.links.length; i++) {
+	      d += "<a href='" + model.links[i].url + "'><div class='text-center link " + divClass + "' >"+model.links[i].text+"</div></a>";
 	    }
 	    model.content.push(d);
 
-
-
-	    x = _.random(0, 6);
-	    for (var i = 0; i < x; i++) {
-	      model.content.push("");
-
-	    }
-	    model.content = davis.darwin([_.shuffle(model.content)], model.stock.content);
-	  };
-
-	controller.cssClasses = function() {
-	    var classes = [];
-	    for (var i = 0; i < 5; i++) {
-	      var color = davis.pick(model.colors);
-	      var name = davis.randomWord() + davis.random(999);
-	      var d = style("." + name, {
-	        "background-color": color.rgb,
-	          "color": color.text
-	      });
-	      classes.push({
-	        "name": name,
-	        "style": d
-	      });
-	    }
-	    model.cssClasses = classes;
-	    //console.log(model.cssClasses);
-	  };
-
-	controller.styles = function() {
-	    model.width = davis.darwin([(70 + davis.random(30)), 100], model.stock.widths);
-	    model.borderRadius = davis.darwin([davis.random(40), 0], model.stock.borderRadii);
-	    model.borderSpacing = davis.darwin([(davis.random(60))], model.stock.borderSpacings);
-	    model.borderWidth = davis.darwin([(davis.random(20)), 0], model.stock.borderWidths);
-
-
-	    model.grey = davis.darwin([davis.randomColor("fullgrey")], model.stock.greys);
-	    model.colors = [];
-	    model.colors.push(davis.darwin([davis.randomColor("full")], model.stock.colors));
-	    model.colors.push(davis.darwin([davis.randomColor("full")], model.stock.colors));
-	    model.colors.push(davis.darwin([davis.randomColor("fullgrey")], model.stock.greys));
-	    model.colors.push(model.grey);
-	    model.bgColor = davis.pick([model.grey, davis.pick(model.colors)]);
-
-
-	    model.frame = [];
-
-	    for (var i = 0; i < 4; i++) {
-	      var frame = davis.sumTo(4);
-	      for (var j = 0; j < frame.length; j++) {
-	        frame[j] *= 3;
-	      }
-	      model.frame.push(frame);
-	    }
-
-	    model.frame = davis.darwin([model.frame], model.stock.frames);
-
-	  };
-
-	controller.love = function(locus, value) {
-	    model.stock[locus] = model.stock[locus] || [];
-	    model.stock[locus].push(value);
-	    return true;
 	  }
 
-	var model = {
-	  appName: "conflexian",
-	  title: "conflexian",
+	  x = _.random(0, 3);
+	  for (var j = 0; j < x; j++) {
+	    d = "";
+	    d += "<h2 class='text-center'>" + davis.random(100) + "%</h2>";
+	    for (var i = 0; i < _.random(1); i++) {
+	      d += "<p class='text-center'>" + davis.randomWord(10 + davis.random(8)) + "</p>";
+	    }
+	    model.content.push(d);
 
-	  bounds: {},
-	  paper: {},
+	  }
 
-	  interests: [
-	    "Luke"
-	    ],
+	  x = _.random(0, 3);
+	  for (var j = 0; j < x; j++) {
+	    d = "";
+	    d += "<h2 class='text-center'>" + davis.random(1000) + " " + davis.pick(model.interests) + "</h2>";
+	    model.content.push(d);
 
-	  links: [
-	  {type:"link", text:"linkedin", url:"http://linkedin.com/in/lucaswadedavis/en/", image:"./images/linkedin-128-black.png"},
-	  {type:"link", text:"twitter", url:"http://twitter.com/lukedavis", image:"./images/twitter-128-black.png"},
-	  {type:"link", text:"github", url:"http://github.com/lucaswadedavis", image:"./images/github-128-black.png"},
-	  {type:"link", text:"blog", url:"http://lucaswadedavis.com", image:"./images/blogger-128-black.png"},
-	  ],
+	  }
 
-	  images128: [
-	  {type:"image128",text:"me",url:"#",image:"./images/headshot-128.png"}
-	  //{type:"image128",text:"mandala time",url:"http://holograf.io",image:"./images/holograf128.png"},
-	  //{type:"image128",text:"mandala time",url:"http://lucaswadedavis.github.io/coloroordinates/",image:"./images/coloroordinates128.png"},
-	  //{type:"image128",text:"mandala time",url:"http://fauxpoe.com",image:"./images/fauxpoe128.png"}
-	  ],
+	  x = _.random(0, 3);
+	  for (var j = 0; j < x; j++) {
+	    d = "";
+	    d += "<h3 class='text-center'>" + davis.pick(model.interests) + "</h3>";
+	    for (var i = 0; i < davis.random(4); i++) {
+	      d += "<p>" + davis.randomWord(10 + davis.random(80)) + "</p>";
+	    }
+	    model.content.push(d);
 
-	    images256: [
-	    {type:"image256",text:"me",url:"#",image:"./images/headshot-256.png"}
-	  //{type:"image256",text:"mandala time",url:"http://holograf.io",image:"./images/holograf256.png"},
-	  //{type:"image256",text:"mandala time",url:"http://lucaswadedavis.github.io/coloroordinates/",image:"./images/coloroordinates256.png"},
-	  //{type:"image256",text:"mandala time",url:"http://fauxpoe.com",image:"./images/fauxpoe256.png"}
-	  ],
+	  }
 
-	    icons: [
-	    {type:"icon", image:"./images/twitter-128.png", url:"http://twitter.com/lukedavis"},
-	    {type:"icon", image:"./images/github-128.png", url:"http://github.com/lucaswadedavis"},
-	    {type:"icon", image:"./images/linkedin-128.png", url:"http://linkedin.com/in/lucaswadedavis/en/"},
-	    {type:"icon", image:"./images/blogger-128.png", url:"http://lucaswadedavis.com"}
-	  ]
+	  x = _.random(0, 6);
+	  for (var j = 0; j < x; j++) {
+	    d = "";
+	    d += "<h3 class='text-center'>";
+	    var links = [];
+	    for (var i = 0; i < model.links.length; i++) {
+	      links.push("<a href='"+model.links[i].url+"'>"+model.links[i].text+"</a>");
+	    }
+	    d += links.join(" | ");
+	    d += "</h3>";
+	    model.content.push(d);
+
+	  }
+
+	  d = "";
+	  for (var i=0;i<model.icons.length;i++){
+	    var icon = model.icons[i];
+	    d += "<a href='"+icon.url+"'><img src='" + icon.image + "' ></a>";
+	  }
+	  model.content.push(d);
+
+
+
+	  x = _.random(0, 6);
+	  for (var i = 0; i < x; i++) {
+	    model.content.push("");
+
+	  }
+	  model.content = davis.darwin([_.shuffle(model.content)], model.stock.content);
+	};
+
+	controller.cssClasses = function() {
+	  var classes = [];
+	  for (var i = 0; i < 5; i++) {
+	    var color = davis.pick(model.colors);
+	    var name = davis.randomWord() + davis.random(999);
+	    var d = style("." + name, {
+	      "background-color": color.rgb,
+	        "color": color.text
+	    });
+	    classes.push({
+	      "name": name,
+	      "style": d
+	    });
+	  }
+	  model.cssClasses = classes;
+	  //console.log(model.cssClasses);
+	};
+
+	controller.styles = function() {
+	  model.width = davis.darwin([(70 + davis.random(30)), 100], model.stock.widths);
+	  model.borderRadius = davis.darwin([davis.random(40), 0], model.stock.borderRadii);
+	  model.borderSpacing = davis.darwin([(davis.random(60))], model.stock.borderSpacings);
+	  model.borderWidth = davis.darwin([(davis.random(20)), 0], model.stock.borderWidths);
+
+
+	  model.grey = davis.darwin([davis.randomColor("fullgrey")], model.stock.greys);
+	  model.colors = [];
+	  model.colors.push(davis.darwin([davis.randomColor("full")], model.stock.colors));
+	  model.colors.push(davis.darwin([davis.randomColor("full")], model.stock.colors));
+	  model.colors.push(davis.darwin([davis.randomColor("fullgrey")], model.stock.greys));
+	  model.colors.push(model.grey);
+	  model.bgColor = davis.pick([model.grey, davis.pick(model.colors)]);
+
+
+	  model.frame = [];
+
+	  for (var i = 0; i < 4; i++) {
+	    var frame = davis.sumTo(4);
+	    for (var j = 0; j < frame.length; j++) {
+	      frame[j] *= 3;
+	    }
+	    model.frame.push(frame);
+	  }
+
+	  model.frame = davis.darwin([model.frame], model.stock.frames);
 
 	};
+
+	controller.love = function(locus, value) {
+	  model.stock[locus] = model.stock[locus] || [];
+	  model.stock[locus].push(value);
+	  return true;
+	}
 
 	var view = {};
 
@@ -321,15 +281,6 @@
 	  }
 	  d += "</div>";
 	  return d;
-	};
-
-	view.style = function() {
-	  var color = davis.pick(model.colors);
-	  var style = "style='";
-	  style += "background-color:" + color.rgb + ";";
-	  style += "color:" + color.text + ";";
-	  style += "'";
-	  return style;
 	};
 
 	view.content = function() {
@@ -622,6 +573,54 @@
 	  }
 	  d += "}";
 	  return d;
+	};
+
+
+
+
+/***/ },
+/* 3 */
+/***/ function(module, exports) {
+
+	module.exports = {
+	  appName: "conflexian",
+	  title: "conflexian",
+
+	  bounds: {},
+	  paper: {},
+
+	  interests: [
+	    "Luke"
+	    ],
+
+	  links: [
+	  {type:"link", text:"linkedin", url:"http://linkedin.com/in/lucaswadedavis/en/", image:"./images/linkedin-128-black.png"},
+	  {type:"link", text:"twitter", url:"http://twitter.com/lukedavis", image:"./images/twitter-128-black.png"},
+	  {type:"link", text:"github", url:"http://github.com/lucaswadedavis", image:"./images/github-128-black.png"},
+	  {type:"link", text:"blog", url:"http://lucaswadedavis.com", image:"./images/blogger-128-black.png"},
+	  ],
+
+	  images128: [
+	  {type:"image128",text:"me",url:"#",image:"./images/headshot-128.png"}
+	  //{type:"image128",text:"mandala time",url:"http://holograf.io",image:"./images/holograf128.png"},
+	  //{type:"image128",text:"mandala time",url:"http://lucaswadedavis.github.io/coloroordinates/",image:"./images/coloroordinates128.png"},
+	  //{type:"image128",text:"mandala time",url:"http://fauxpoe.com",image:"./images/fauxpoe128.png"}
+	  ],
+
+	    images256: [
+	    {type:"image256",text:"me",url:"#",image:"./images/headshot-256.png"}
+	  //{type:"image256",text:"mandala time",url:"http://holograf.io",image:"./images/holograf256.png"},
+	  //{type:"image256",text:"mandala time",url:"http://lucaswadedavis.github.io/coloroordinates/",image:"./images/coloroordinates256.png"},
+	  //{type:"image256",text:"mandala time",url:"http://fauxpoe.com",image:"./images/fauxpoe256.png"}
+	  ],
+
+	    icons: [
+	    {type:"icon", image:"./images/twitter-128.png", url:"http://twitter.com/lukedavis"},
+	    {type:"icon", image:"./images/github-128.png", url:"http://github.com/lucaswadedavis"},
+	    {type:"icon", image:"./images/linkedin-128.png", url:"http://linkedin.com/in/lucaswadedavis/en/"},
+	    {type:"icon", image:"./images/blogger-128.png", url:"http://lucaswadedavis.com"}
+	  ]
+
 	};
 
 
